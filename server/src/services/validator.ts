1cf8a8832cdb8a0928dab33cabc356ba373f0d68
Roman Dovhan <dovhan.rg@gmail.com>
2024-03-06 04:18:19 +0200

    convert position_id to number while validating

--- server/src/services/validator.ts
+++ server/src/services/validator.ts
@@ -10,7 +10,7 @@ export const userSchema = z.object({
     name: nameSchema,
     email: z.string().trim().max(250, 'Max email length is 250 characters').email({message: 'Invalid email address'}),
     phone: z.string().regex(uaPhoneNumberRegex, 'Invalid number. Number must be in format +380000000000'),
-    position_id: z.number().positive(),
+    position_id: z.coerce.number().positive(),
     password: z.string().min(4).max(20),
 });
 
