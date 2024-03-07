97cc45b8f2688d8892c827c7c48ee383b7568a78
Roman Dovhan <dovhan.rg@gmail.com>
2024-03-07 05:56:37 +0200

    fixed address

--- server/src/consts.ts
+++ server/src/consts.ts
@@ -2,7 +2,7 @@
 const IP_ADDR = process.env.IP_ADDR || 'localhost';
 const API_PORT = process.env.API_PORT || 40000;
 
-const api_url = process.env.NODE_ENV === 'development' ? '' : `https://164.92.235.193`;
+const api_url = process.env.NODE_ENV === 'development' ? '' : `http://164.92.235.193`;
 
 export {
     api_url,
